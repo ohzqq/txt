687147dd2c1bdf5790a409120c7d8c6946c070e2
ohzqq <iamchurkey@gmail.com>
2024-03-08 13:36:43 -0500

    fuzzy tests pass

--- analyzer_test.go
+++ analyzer_test.go
@@ -56,6 +56,83 @@ func TestKeywordAnalyzer(t *testing.T) {
 	}
 }
 
+func TestFuzzySearch(t *testing.T) {
+	var testStrings = map[string]int{
+		``:                    0,
+		`!@@#$$%%^`:           1,
+		`quick brown fox`:     1,
+		`QUICK BROWN FOX`:     1,
+		`the quick brown fox`: 1,
+		`the quick brown fox jumped and is running`:     1,
+		`the quick, brown fox jumped! (and is running)`: 1,
+		`The quick, brown fox jumped! (And is running)`: 1,
+	}
+
+	ana := NewAnalyzer()
+	ana.Keywords()
+	for test, want := range testStrings {
+		tokens, err := ana.Tokenize(test)
+		if err != nil {
+			println(err.Error())
+		}
+		numToks := len(tokens)
+		if want != numToks {
+			t.Errorf("got %d tokens, wanted %d\n", numToks, want)
+		}
+		m := tokens.FuzzyFind("row")
+		switch test {
+		case ``, `!@@#$$%%^`:
+			if m.Len() != 0 {
+				t.Errorf("got %d tokens, wanted %d\n", m.Len(), want)
+			}
+		default:
+			if want != m.Len() {
+				t.Errorf("got %d tokens, wanted %d\n", m.Len(), want)
+			}
+		}
+	}
+}
+
+func TestFuzzySearchNormalized(t *testing.T) {
+	var testStrings = map[string]int{
+		``:                    0,
+		`!@@#$$%%^`:           0,
+		`quick brown fox`:     3,
+		`QUICK BROWN FOX`:     3,
+		`the quick brown fox`: 4,
+		`the quick brown fox jumped and is running`:      8,
+		`the quick, brown fox jumped! (and is running)`:  8,
+		`The quick, brown fox jumping! (And is running)`: 8,
+	}
+
+	ana := NewAnalyzer(
+		strings.ToLower,
+		AlphaNum,
+	)
+	for test, want := range testStrings {
+		tokens, err := ana.Tokenize(test)
+		if err != nil {
+			println(err.Error())
+		}
+		numToks := len(tokens)
+		if want != numToks {
+			t.Errorf("got %d tokens, wanted %d\n", numToks, want)
+		}
+
+		m := tokens.FuzzyFind("ing")
+		switch test {
+		case `the quick brown fox jumped and is running`, `the quick, brown fox jumped! (and is running)`:
+			if m.Len() != 1 {
+				t.Errorf("got %d tokens, wanted %d\n", m.Len(), want)
+			}
+		case `The quick, brown fox jumping! (And is running)`:
+			if m.Len() != 2 {
+				t.Errorf("got %d tokens, wanted %d\n", m.Len(), want)
+			}
+		}
+	}
+}
+
 func TestAnalyzerToLower(t *testing.T) {
 	var testStrings = map[string]int{
 		``:                    0,
